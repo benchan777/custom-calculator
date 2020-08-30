e134bfda49b4a34844d8661b539b9d7740442182
benchan777 <benchan777@gmail.com>
2020-08-30 01:24:34 -0700

    more comment updates

--- custom_calculator.py
+++ custom_calculator.py
@@ -21,8 +21,8 @@ while True:
     except ValueError:
         print("Please enter a number.") #asks user to enter a number if they enter something that is unable to be converted into an integer
 #performs the calculations needed whether the user chooses siglo or plate
-siglo_craft_count = (amount_of_item / 3)
-number_of_kits = (siglo_craft_count * 60)
+siglo_craft_count = (amount_of_item / 3) #calculates how many total craft counts are needed for specified number of siglos. since 3 siglos are crafted per craft action, the total number of craft actions needed is total number of siglos wanted divided by 3
+number_of_kits = (siglo_craft_count * 60) #total number of refining kits needed. 60 kits are needed per siglo craft so the total number of kits is 60*number of craft actions
 plate_craft_count = (amount_of_item / 3)
 if item_type == "siglo": #performs the calculations needed if the user specifically enters siglo
     number_of_talents = (siglo_craft_count * 5)
